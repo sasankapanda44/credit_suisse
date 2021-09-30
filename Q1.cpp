3a03e843b6cd5af04fd22f6c381ed7628ed4737c
sasankapanda44 <56378185+sasankapanda44@users.noreply.github.com>
2021-10-01 00:27:24 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long int n;
+	cin>>n;
+	long long int z=(n+2)/3;
+	cout<<z+(z-1)<<endl;
+}
